6b00043bf268160a7eb6341b090e49d2fa60a469
sathwikpreetham27 <132142012+sathwikpreetham27@users.noreply.github.com>
2023-05-14 22:46:50 +0530

    Add files via upload

---
+++ ncert/11/16/3/4/Code/A2.py
@@ -0,0 +1,23 @@
+# Total number of cards in a pack
+total_cards = 52
+
+# (a) Number of points in the sample space
+sample_space = total_cards
+print("Number of points in the sample space:", sample_space)
+
+# (b) Probability of selecting an ace of spades
+aces_of_spades = 1
+prob_ace_of_spades = aces_of_spades / total_cards
+print("Probability of selecting an ace of spades:", prob_ace_of_spades)
+
+# (c) Probability of selecting
+# (i) an ace
+aces = 4
+prob_ace = aces / total_cards
+print("Probability of selecting an ace:", prob_ace)
+
+# (ii) a black card
+black_cards = 26
+prob_black_card = black_cards / total_cards
+print("Probability of selecting a black card:", prob_black_card)
+
