353b0f4a1bc31f930916447502c3a696f69a09db
Karthikeya210 <139639550+Karthikeya210@users.noreply.github.com>
2023-08-23 14:51:28 +0530

    Add files via upload

---
+++ ncert/12/13/5/8/codes/main.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import binom
+
+# Sample size
+simlen = 10000
+
+# Possible outcomes
+k_values = np.arange(0, 7)
+
+# Generate X1 and X2 without explicit loops
+y = np.random.randint(0, 2, size=(6, simlen))
+
+# Calculate X without loops
+X = np.sum(y, axis=0)
+
+# Find the frequency of each outcome
+unique, counts = np.unique(X, return_counts=True)
+
+# Simulated probability
+psim = counts / simlen
+
+#Theoretical Probability
+p = 0.5 
+n = 6    
+#We can use binom to create binomial distribution
+rv = binom(n, p)
+
+#Make the list combinations with rv.pmf function
+combinations = rv.pmf(k_values)
+
+# Find the most likely outcome
+most_likely_outcome = np.argmax(combinations)
+
+print(f"Therefore the most likely outcome is for X = {most_likely_outcome}")
+
+# Plotting
+plt.stem(k_values, psim, markerfmt='o', linefmt='C0-', use_line_collection=True, label='Simulation')
+plt.stem(k_values, combinations, markerfmt='o', linefmt='C1-', use_line_collection=True, label='Analysis')
+plt.xlabel('$k$')  # Use 'k' instead of 'n'
+plt.ylabel('$p_{X}(k)$')  # Use 'k' instead of 'n'
+plt.legend()
+plt.grid()
+
+# Save or display the plot
+plt.savefig('/home/karthikeya/Desktop/Probability/New_assignment1/figs/figure1.png')
+plt.show()
+
